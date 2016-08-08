3c13111550516b460b312a47f9711fb5988a9ec1
Alexis Sellier <alexis@cloudhead.io>
2016-08-08 19:05:48 +0200

    Key binding to suspend machine

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -2,6 +2,10 @@ import XMonad
 import XMonad.Config.Desktop
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run
+
+import Data.Map (Map)
+import qualified Data.Map as Map
 
 main :: IO ()
 main = xmonad =<< statusBar "xmobar" barConfig toggleStrutsKey desktopConfig
@@ -10,6 +14,7 @@ main = xmonad =<< statusBar "xmobar" barConfig toggleStrutsKey desktopConfig
      , normalBorderColor  = "black"
      , borderWidth        = 2
      , handleEventHook    = mconcat [docksEventHook, handleEventHook def]
+     , keys               = myKeys
      }
 
 barConfig :: PP
@@ -30,3 +35,11 @@ barConfig = xmobarPP
 
 toggleStrutsKey :: XConfig t -> (KeyMask, KeySym)
 toggleStrutsKey XConfig { XMonad.modMask = modMask } = (modMask, xK_b)
+
+myKeys :: XConfig Layout -> Map (KeyMask, KeySym) (X ())
+myKeys conf@XConfig { XMonad.modMask = modMask } =
+    Map.union (XMonad.keys def conf) ks
+  where
+    ks = Map.fromList
+       [ ((modMask, xK_F12), safeSpawn "systemctl" ["suspend"])
+       ]
