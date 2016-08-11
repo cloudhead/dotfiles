7c1d8bda752a8d7090dd7685e9be6b7a6b79de80
Alexis Sellier <alexis@cloudhead.io>
2016-08-12 00:53:48 +0200

    Revert xmonad to 'st', dvtm can go to hell

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -16,7 +16,7 @@ main = do
     home <- getHomeDirectory
     xmproc <- spawnPipe "xmobar"
     xmonad $ desktopConfig
-        { terminal           = "st dvtm -M -h 1000 -d 0"
+        { terminal           = "st"
         , focusedBorderColor = "#555"
         , normalBorderColor  = "black"
         , borderWidth        = 2
