d21f92d82ce3e1f626a5a891cb9ae786ebe6e40d
Alexis Sellier <alexis@cloudhead.io>
2016-08-12 12:36:58 +0200

    Minor tweaks to the console font

--- .st/config.h
+++ .st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char font[] = "Inconsolata:pixelsize=32:antialias=true:autohint=true:dpi=150";
+static char font[] = "Inconsolata:pixelsize=30:antialias=true:autohint=false:dpi=150";
 static int borderpx = 10;
 #define histsize 4000
 
