3bd897b8359e29e8c0c08abc29e2dfc593c76b96
Alexis Sellier <alexis@cloudhead.io>
2016-08-21 23:54:12 +0200

    Better size

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -78,7 +78,7 @@ myKeys home conf@XConfig { XMonad.modMask = modMask } =
        ]
     dmenu :: X ()
     dmenu = safeSpawn
-        "dmenu_run" [ "-fn", "monospace:size=13"
+        "dmenu_run" [ "-fn", "monospace:size=12"
                     , "-i"
                     , "-h",  "40"
                     , "-nb", "black"
