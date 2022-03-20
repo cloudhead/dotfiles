772728699ac1c9a061bc0f707d5fefab598579b4
Odysseas Lamtzidis <odyslam@gmail.com>
2022-03-20 21:56:17 +0200

    feat: populate files script

---
+++ populate.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+cp .vimrc ~/.vimrc
+cp .zshrc ~/.zshrc
+cp .zshenv ~/.zshenv
+cp .tmux.conf ~/tmux.conf
+cp .gitconfig ~/.gitconfig
+cp .gitignore ~/.gitignore
+cp .dircolors ~/.dircolors
+cp -R .vim ~/.vim
+cp -r .config/nvim/ ~/.config/nvim
+cp -r .config/coc/ ~/.config/coc
+
--- scripts/brew.sh
+++
@@ -1,8 +0,0 @@
-install_brew() {
-    if test "$(uname)" != "Darwin" ; then
-      echo Skipping brew as this device is not running macOS
-      return
-    fi
-    brew bundle --file scripts/Brewfile
-}
-
